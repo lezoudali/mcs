2c335baae2cf6dd7d99d361716a560c00f7275be
Lezou Dali <lezou.dali@gmail.com>
2015-06-23 12:35:12 -0400

    add user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  before_create :generate_authentication_token!
+
   include SoftDeletable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -6,4 +8,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :name, presence: true, length: {in: 2..35}
+  validates :auth_token, uniqueness: true
+
+  def generate_authentication_token!
+    begin 
+      self.auth_token = Devise.friendly_token
+    end while self.class.exists?(auth_token: auth_token)
+  end
 end
