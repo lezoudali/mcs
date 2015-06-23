f4588eb625fbfa7b3e8407b27db42180423db8d9
Lezou Dali <lezou.dali@gmail.com>
2015-06-23 14:10:05 -0400

    adds sessions controller create action

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/' do 
     scope module: :v1 do 
       resources :users, except: :index
+      resources :sessions, only: [:create, :destroy]
     end
   end
 end
