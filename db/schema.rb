cab054d222ae3c03046e43a80f96928e42b5002c
Mykle Nero <mykle.nero@gmail.com>
2015-07-27 12:03:20 -0400

    Added index_num

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706223422) do
+ActiveRecord::Schema.define(version: 20150727150111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 20150706223422) do
     t.integer  "mcs_admin_id"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "index_num"
   end
 
   add_index "videos", ["mcs_admin_id"], name: "index_videos_on_mcs_admin_id", using: :btree
