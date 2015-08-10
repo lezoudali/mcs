89bc756497fbf4fc782d430eb2b8e040e434c87e
Lezou Dali <lezou.dali@gmail.com>
2015-08-10 14:28:47 -0400

    counts in index

--- app/serializers/video_index_serializer.rb
+++ app/serializers/video_index_serializer.rb
@@ -1,4 +1,17 @@
 class VideoIndexSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :source_url, :views, :num_comments,
-  :num_shares, :index_num, :poster_image_url, :fashion_model_name
+  :num_shares, :index_num, :poster_image_url, :fashion_model_name , :num_likes, :num_purchases
+
+
+  def num_likes
+    rand(150...200)
+  end
+
+  def num_purchases
+    rand(50..100)
+  end
+
+  def num_shares
+    rand(50..100)
+  end
 end
