66d1f039f85411b51ec5c98863ff3c83473f3dbf
Mykle Nero <mykle.nero@gmail.com>
2015-09-08 13:47:50 -0400

    - Added route, action, and tests for video_likes index. - Moved tests for users session_controller

--- app/controllers/api/v1/video_likes_controller.rb
+++ app/controllers/api/v1/video_likes_controller.rb
@@ -1,12 +1,23 @@
 class Api::V1::VideoLikesController < ApplicationController
-  before_action :authenticate_with_token!,  only: [:create, :destroy]
+  before_action :authenticate_with_token!,  only: [:index, :create, :destroy]
   before_action :set_video, only: :create
+
+  def index
+    video_params = { user_id: current_user.id, video_id: params[:video_id]}
+    likes = VideoLike.where(video_params)
+
+    if likes.length == 0
+      render json: {}, status: 404
+    else
+      render json: {video_likes: likes}
+    end
+  end
 
   def create
     video_like = @video.video_likes.build
     video_like.user = current_user
 
-    if video_like.save 
+    if video_like.save
       render json: {video_id: video_like.video.id, user_id: video_like.user.id}, status: 201
     else
       render json: { errors: video_like.errors }, status: 422
--- spec/controllers/api/v1/users/sessions_controller_spec.rb
+++ spec/controllers/api/v1/users/sessions_controller_spec.rb
@@ -1,30 +1,31 @@
 require 'spec_helper'
 
-describe Api::V1::SessionsController do
-  before(:each) do 
+describe Api::V1::Users::SessionsController do
+  before(:each) do
     @user = create_user
+    @request.env["devise.mapping"] = Devise.mappings[:user]
   end
 
-  describe "POST #create" do 
-    context "when the credentials are correct" do 
-      before(:each) do 
+  describe "POST #create" do
+    context "when the credentials are correct" do
+      before(:each) do
         credentials = { email: @user.email, password: "12345678"}
         post :create, { session: credentials }
       end
 
-      it "returns the user record corresponding to the given credentials" do 
+      it "returns the user record corresponding to the given credentials" do
         @user.reload
         expect(json_response[:user][:auth_token]).to eql @user.auth_token
       end
 
       it { should respond_with 200 }
 
-      context "when the credentials are incorrect" do 
-        before(:each) do 
+      context "when the credentials are incorrect" do
+        before(:each) do
           credentials = { email: @user.email, password: "invalidpassword"}
           post :create, { session: credentials }
 
-          it "returns a json with an error" do 
+          it "returns a json with an error" do
             expect(json_response[:errors]).to eql "Invalid email and/or passowrd"
           end
 
@@ -34,8 +35,8 @@ describe Api::V1::SessionsController do
     end
   end
 
-  describe "DELETE #destroy" do 
-    before(:each) do 
+  describe "DELETE #destroy" do
+    before(:each) do
       sign_in @user
       delete :destroy, id: @user.auth_token
     end
